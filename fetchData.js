1561f9e34b13fc75ac9435ef920d6a9320340894
Marcin Kołodziej <marcin1.kolodziej@gmail.com>
2022-10-08 16:09:05 +0200

    basic rendering added

---
+++ fetchData.js
@@ -0,0 +1,21 @@
+const fetchData = (url, options = {}) => {
+    const {
+        startCallback = () => console.log(`[${url}] Start fetching`),
+        catchCallback = (error) => console.error('Failed to fetch data', error),
+        endCallback = () => console.log(`[${url}] Stop fetching`)
+    } = options
+    
+    startCallback()
+
+    return fetch(url)
+        .then((response) => response.json())
+        // .then((data) => console.log(data))
+        .catch(catchCallback)
+        .finally(endCallback)
+}
+
+fetchData('./data.json')
+    .then((data) => console.log(data))
+
+fetchData('./data2.json', { startCallback: () => console.log('Statr fetching data2.json')})
+    .then((data) => console.log(data))
---
+++ script.js
@@ -0,0 +1,33 @@
+class ToDo {
+    constructor() {
+        this.container = document.body
+        this.tasks = [
+            {
+                text: "Wynieś śmieci"
+            }
+        ]
+    }
+
+    init(){
+        this.render()
+    }
+
+    render() {
+        this.container.innerHTML = ''
+
+        const ul = document.createElement('ul')
+
+        this.tasks.forEach((task) => {
+            const li = document.createElement('li')
+
+            li.innerText = task.text
+
+            ul.appendChild(li)
+        });
+
+        this.container.appendChild(ul)
+    }
+}
+
+const todo1 = new ToDo()
+todo1.init()
